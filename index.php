59c8ccf7ba95800bf6ac79c1d0623af8ad8b57a1
javier <jo561664@gmail.com>
2020-03-31 19:30:32 -0500

    Modificacion en el archivo index ya que este presentaba errores en al validar la informacion del formulario

--- index.php
+++ index.php
@@ -12,16 +12,16 @@
 				<h2>Login</h2><br>
 				<form action="">
 					<input class="form-control input-sm text-center" type="text" placeholder="Usuario"></iput><br>
-					<input class="form-control input-sm text-center" type="text" placeholder="Contraseña"></iput><br>
+					<input class="form-control text-center" type="text" placeholder="Contraseña"></iput><br>
 					<input class="btn btn-primary" type="submit" value="Iniciar">
 				</form>
 			</div>	
 		</div>
 	</div>
 	<?php 
-	if (isset($_POST['foto']) and isset($_POST['nombre']) and and isset($_POST['apellido']) and and isset($_POST['email']) and isset($_POST['clave'])):
+	if (isset($_POST['foto']) && isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['email']) && isset($_POST['clave'])):
 		require_once "./proceso/insert.php";
-
+	endif;
 	 ?>
 </body>
 </html>
