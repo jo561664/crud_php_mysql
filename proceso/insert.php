26d5c545b8573421c640da4be9c3cae82fab5d1a
javier <jo561664@gmail.com>
2020-04-01 17:58:16 -0500

    He movido estas lineas de codigo ya que hera necesario

--- proceso/insert.php
+++ proceso/insert.php
@@ -11,13 +11,13 @@
 
 	if (move_uploaded_file($_FILES['foto']['tmp_name'], $ruta)):
 		$query = "INSERT INTO tbl_usuario(foto, nombre, apellido, email, clave)VALUES('$foto', '$nombre','$apellido', '$email', '$clave')";
+		if(SQL($query)):
+			echo "<h4>Los datos de ".$nombre." se guardaron exitosamente.</h4>";
+		else:
+			echo "<h4>Ocurrio un error en el servidor</h4>";
+		endif;
 	else:
 		echo "<h4>Ocurrio un error la foto no se subio</h4>";
 	endif;
 
-	if(SQL($query)):
-		echo "<h4>Los datos de ".$nombre." se guardaron exitosamente.</h4>";
-	else:
-		echo "<h4>Ocurrio un error en el servidor</h4>";
-	endif;
 
