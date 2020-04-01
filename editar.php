9e19122b4b77b41068ca92dbe7407cd93868f8f4
javier <jo561664@gmail.com>
2020-04-01 17:48:12 -0500

    He agregado codigo php en el archivo editar.php el cual consiste en llenar los campos del formulario actualizar

--- editar.php
+++ editar.php
@@ -5,22 +5,29 @@
 	<title>Actualizar registro</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="./librerias/bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" href="./librerias/style.css">
+	<link rel="stylesheet" href="./style.css">
 </head>
 <body>
+	<?php 
+		if (isset($_GET['id'])):
+			$id = $_GET['id'];
+			require_once "./php/conexion.php";
+			$query = "SELECT * FROM tbl_usuario WHERE doc = '$id'";
+			$consulta = SQL($query);
+			$fila = $consulta->fetch_object();
+	 ?>
 	<div class="container">
-		<h2 class="text-center">Formulario de registro</h2>
+		<h2 class="text-center">Actualizar registro</h2>
 		<br>
-		<form action="" method="POST" autocomplete="off" enctype="multipart/form-data">
+		<form action="proceso/update.php" method="POST" autocomplete="off" enctype="multipart/form-data">
 			<div class="form_">
-				<label for="foto">Foto</label>
-				<input class="form-control input-sm" type="file" id="foto" name="foto"><br>
+				<input type="text" hidden="" name="id" value="<?php echo @$fila->doc; ?>">
 				<label for="nombre">Nombre</label>
-				<input class="form-control input-sm" type="text" id="nombre" name="nombre"><br>
+				<input class="form-control input-sm" type="text" id="nombre" name="nombre" value="<?php echo @$fila->nombre; ?>"><br>
 				<label for="apellido">Apellido</label>
-				<input class="form-control input-sm" type="text" id="apellido" name="apellido"><br>
+				<input class="form-control input-sm" type="text" id="apellido" name="apellido" value="<?php echo @$fila->apellido; ?>"><br>
 				<label for="email">E-mail</label>
-				<input class="form-control input-sm" type="email" id="email" name="email"><br>
+				<input class="form-control input-sm" type="email" id="email" name="email" value="<?php echo @$fila->email; ?>"><br>
 			</div>
 			<div class="form_">
 				<h4>Opcional</h4>
@@ -34,5 +41,6 @@
 			<input class="btn btn-warning" type="submit" value="Actualizar">
 		</form>
 	</div>
+<?php endif; ?>
 </body>
 </html>
